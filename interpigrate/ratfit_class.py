8f50c1cc9b5164ff970715a746ee40500a525abe
JLS <jonathan.sievers@gmail.com>
2024-09-05 13:05:22 -0400

    tweaked in-class ratfit

--- interpigrate/ratfit_class.py
+++ interpigrate/ratfit_class.py
@@ -13,7 +13,33 @@ def fit_rat(x,y,nump,nq):
     fitp=np.linalg.inv(A)@y
     return fitp
 
-x=np.linspace(-1,1,7)
-y=np.exp(-0.5*x**2)
-fit=fit_rat(x,y,3,3)
+def rat_eval(fitp,x,nump,nq):
+    top=0
+    for i in range(nump+1):
+        top=top+x**i*fitp[i]
+    bot=1
+    for i in range(nump+1,len(fitp)):
+        ii=i-nump
+        bot=bot+x**ii*fitp[i]
+    return top/bot
 
+
+nump=3
+nq=5
+x=np.linspace(-1,1,nump+nq+1)
+y=np.exp(-0.5*x**2)
+fit=fit_rat(x,y,nump,nq)
+assert(1==0)
+xx=np.linspace(-15,15,1001)
+yy=rat_eval(fit,xx,nump,nq)
+yy_true=np.exp(-0.5*xx**2)
+
+#pfit=np.polyfit(x,y,10)
+#yyp=np.polyval(pfit,xx)
+
+plt.clf()
+plt.plot(xx,yy)
+plt.plot(x,y,'.')
+plt.plot(xx,yy_true)
+#plt.plot(xx,yyp)
+plt.show()
