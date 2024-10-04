061091c175d47a36b75277554fee3f3b9620e823
JLS <jonathan.sievers@gmail.com>
2024-10-04 10:43:24 -0400

    updated in-class detailed mcmc to do equivalent sigma-based confidence intervals for low/high-T chains

--- modelling/mcmc_gauss_detailed_class.py
+++ modelling/mcmc_gauss_detailed_class.py
@@ -1,7 +1,48 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.special import erfc
 plt.ion()
 
+
+def nsig2prob(nsig):
+    #use the complementary error function to get the one-sided probability
+    #given the number of sigma we want.
+    return erfc(nsig/np.sqrt(2))/2
+def get_lims(chain,wt,nsig):
+
+    npar=chain.shape[1] #number of parameters
+    pthresh=nsig2prob(nsig) #find the equivalent probability fraction
+    eplus=np.zeros(npar)
+    eminus=np.zeros(npar)
+    for i in range(npar):
+        #first, sort the chain based on the variable we want
+        inds=np.argsort(chain[:,i])
+        param_sorted=chain[inds,i] #this returns the sorted samples
+        #now sort the weights based on the chain sample
+        wt_sorted=wt[inds]
+        #now we can find the parameter value that has
+        #pthresh probability below it
+        wt_cumsum=np.cumsum(wt_sorted)
+        targ=wt_cumsum[-1]*pthresh
+        #go find the max parameter value in the chain
+        #below which you have your desired probability
+        #fraction.  If this fails, that means you don't
+        #have any samples out there, so return NaN
+        try:
+            myind=np.max(np.where(wt_cumsum<targ)[0])
+            eminus[i]=param_sorted[myind]
+        except:
+            eminus[i]=np.nan
+
+        #repeat, but from the other side
+        targ=wt_cumsum[-1]*(1-pthresh)
+        try:
+            myind=np.min(np.where(wt_cumsum>targ)[0])
+            eplus[i]=param_sorted[myind]
+        except:
+            eplus[i]=np.nan
+    return eminus,eplus
+                            
 def gauss(pars,x):
     amp=pars[0]
     x0=pars[1]
@@ -90,7 +131,11 @@ for i in range(ntrial):
     r=y_trial-y
     chi_trial[i]=r@Ninv@r
 
+ncut=1000
 chain,chivec=run_chain(p_trial,gauss_chisq,y,x,dpars,Ninv,L)
+#we'll remove burn-in, and just assume 1000 samples is good enough
+chain=chain[ncut:,:]
+chivec=chivec[ncut:]
 #chain2,chivec2=run_chain(p_trial,gauss_chisq,y,x,dpars,Ninv,10*L)
 #chain3,chivec3=run_chain(p_trial,gauss_chisq,y,x,dpars,Ninv,0.1*L)
 print('accept probability is ',1-np.mean(np.diff(chivec)==0))
@@ -109,8 +154,17 @@ for i in range(npar):
     psig=np.sqrt(wt_sqr-wt_mean**2)
     print('new value and uncertainty for parameter ',i,' are ',wt_mean,psig)
 
-T=5
-chain_hot,chivec_hot=run_chain(p_trial,gauss_chisq,y,x,dpars,Ninv/T**2,L,nsamp=50000)
+sig_targ=3.5
+#for a normal temperature chain, all samples are equally weighted
+pminus,pplus=get_lims(chain,0*chivec+1,sig_targ)
+for i in range(len(pminus)):
+    print(sig_targ,'-sigma confidence interval on param ',i,' is ',pminus[i],pplus[i],' with half-width ',(pplus[i]-pminus[i])/2)
+
+    
+T=3
+chain_hot,chivec_hot=run_chain(p_trial,gauss_chisq,y,x,dpars,Ninv/T**2,L,nsamp=10000)
+chain_hot=chain_hot[ncut:,:] #once again, remove burn-in
+chivec_hot=chivec_hot[ncut:]
 delta_chi=chivec_hot-chivec_hot.min()
 wt=np.exp(-0.5*(T**2-1)*delta_chi)
 print('high-T mean: ',np.mean(chain_hot,axis=0))
@@ -123,9 +177,11 @@ for i in range(npar):
     print('new value and uncertainty for parameter ',i,' are ',wt_mean,psig)
 
 wt_sort=wt.copy()
-wt.sort()
-wt_cumsum=np.cumsum(wt)
+wt_sort.sort()
+wt_cumsum=np.cumsum(wt_sort)
 wt_cumsum=wt_cumsum/wt_cumsum[-1]
 
-#let's do 5-sigma errors for amplitude
-#mywt,myval=np.sortrows(np.hstack([chain_hot[:,0],wt]),0)
+#let's do our target  sigma errors for amplitude
+pminus,pplus=get_lims(chain_hot,wt,sig_targ)
+for i in range(len(pminus)):
+    print(sig_targ,'-sigma hot chain confidence interval on param ',i,' is ',pminus[i],pplus[i],' with half-width ',(pplus[i]-pminus[i])/2)
